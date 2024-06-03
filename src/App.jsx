b8bb71f65277f71dfca657f9eebdbc2f73dc5568
UZBEKGIRL <rfarangiz1305@gmail.com>
2024-06-03 11:12:52 +0500

    Img component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Img from './components/Img/Img'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Img width = "100px" height= "100px" radius = "15px" img = "https://avatars.mds.yandex.net/i?id=bc690d199549d835bd3edb77fcf3eb113c2d931b-11951022-images-thumbs&n=13"></Img>
     </>
   )
 }
